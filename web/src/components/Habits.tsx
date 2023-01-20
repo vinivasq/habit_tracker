e284c2ea93f2c03c5e7ad44706162ecfa7170fd5
Vinícius Vasques <vinivasq@hotmail.com>
2023-01-20 11:28:01 -0300

    estrutura front-end da homepage

--- web/src/components/Habits.tsx
+++
@@ -1,9 +0,0 @@
-interface habitsProps {
-    completed: number
-}
-
-export function Habit(props: habitsProps) {
-    return (
-        <div className="bg-slate-600">{props.completed}</div>
-    )
-}
