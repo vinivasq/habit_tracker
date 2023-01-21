a0978733a7c46c2ef26bfc6a013ffae4f5afe5c7
Vinícius Vasques <vinivasq@hotmail.com>
2023-01-21 17:15:01 -0300

    transições e foco

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -56,7 +56,7 @@ export function NewHabitForm(){
                 type="text"
                 id="title"
                 placeholder="ex.: Me exercitar, Beber água, etc..."
-                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
                 autoFocus
                 value={title}                
                 onChange={event => (setTitle(event.target.value))}
@@ -70,12 +70,12 @@ export function NewHabitForm(){
                 {availableWeekDays.map((weekDay, i) => (
                     <Checkbox.Root 
                         key={weekDay} 
-                        className='flex items-center gap-3  group'
+                        className='flex items-center gap-3  group focus:outline-none'
                         checked={weekDays.includes(i)}
                         onCheckedChange={() => handleToggleWeekDay(i)}
                     >
                         <div 
-                            className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+                            className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'
                         >
                             <Checkbox.Indicator>
                                 <Check
@@ -92,7 +92,10 @@ export function NewHabitForm(){
                 ))}
             </div>
 
-            <button type="submit" className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500">
+            <button 
+                type="submit" 
+                className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+                >
                 <Check size={20} weight="bold" />
                 Confirmar
             </button>
